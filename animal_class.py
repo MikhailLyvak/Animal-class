4d0aebf378b814adf9e596536652dbb08897e089
DimaS <154427437+SamolinkoD@users.noreply.github.com>
2023-12-29 19:23:31 +0200

    Update animal_class.py
    Done

--- animal_class.py
+++ animal_class.py
@@ -1,4 +1,7 @@
 # Пиши свій код тут
+from turtle import position
+
+
 class Animal:
     def __init__(self, name: str, current_fead: int, max_fead: int, speed: int, loss_fead_per_km: int, position: int = 0):
         self.name = name
@@ -11,19 +14,28 @@ class Animal:
     def __str__(self) -> str:
         return f"{self.name} Feed: {self.current_fead}/{self.max_fead} Speed: {self.speed} Loss: {self.loss_fead_per_km} | Position: {self.position}"
         
-    def can_move_to (self, distance: int, animal):
-        if self.current_fead >= animal.current_fead:
-            return False
+    def can_move_to (self, distance: int, animal) -> bool:
+        required_feed = distance * self.loss_fead_per_km
+        return animal.current_fead >= required_feed
+    
+    def move_to(self, distance: int) -> None:
+        if self.can_move_to(distance, self):
+             self.position += distance
+             self.current_fead -= distance * self.loss_fead_per_km
+             print(f"Тваринка перемістилась на відстань {distance}.")
         else:
-            self.current_fead += distance - self.loss_fead_per_km * distance / 100
-            self.position += distance
-            return True
+            print("Not enough feed")
+
+    def feed (self, calories: int) -> None:
+        if self.current_fead + calories <= self.max_fead:
+            self.current_fead += calories
+        else:
+            print("Too much feed")
         
 
-    
+
 
 cat = Animal("Cat", 70, 200, 20, 2)
-
-print( cat)
-print(cat.can_move_to(10, cat)) # Має повернути: True
-print(cat.can_move_to(100, cat)) # Має повернути: False
+print(cat)
+cat.move_to(30)
+print(cat)
